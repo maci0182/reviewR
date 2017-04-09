f3f6d7e1a13c2662cd031ddb584cd9c1b1065023
maci0182 <brucemacinnis@Bruces-MacBook-Pro.local>
2017-04-09 18:08:58 -0400

    Layout of index.html

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -20,6 +20,7 @@ var app = {
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        console.log("Device Ready");
     },
 
     // deviceready Event Handler
